e636f0137e07a20db16a09cba8c5f0f4f153c864
galvao.dev <inosuke567765@gmail.com>
2024-09-06 21:10:11 -0300

    feat: alterada a estrutura da base de dados

--- js/bd.js
+++ js/bd.js
@@ -4,79 +4,149 @@ export const bd = [
     description:
       "JavaScript é uma linguagem de programação que permite implementar funcionalidades complexas em páginas web.",
     helloWorld: "console.log('Olá, Mundo!')",
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/javascript",
-    linkDocumentacao: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/javascript",
+        docs: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=1-w1RfGIov4&list=PLHz_AreHm4dlsK3Nr9GVvXCbpQyHQl1o1&pp=iAQB",
+      ],
+    },
   },
   {
     name: "Python",
     description:
       "Python é uma linguagem de programação de alto nível, conhecida por sua sintaxe simples e legibilidade.",
     helloWorld: "print('Olá, Mundo!')",
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/python",
-    linkDocumentacao: "https://docs.python.org/3/",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/python",
+        docs: "https://docs.python.org/3/",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=S9uPNppGsGo&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0",
+      ],
+    },
   },
   {
     name: "Java",
     description:
       "Java é uma linguagem de programação orientada a objetos, amplamente utilizada em aplicações corporativas.",
     helloWorld: "System.out.println('Olá, Mundo!');",
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/java",
-    linkDocumentacao: "https://docs.oracle.com/javase/8/docs/api/",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/java",
+        docs: "https://docs.oracle.com/javase/8/docs/api/",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=VKjFuX91G5Q&list=PL62G310vn6nFIsOCC0H-C2infYgwm8SWW",
+      ],
+    },
   },
   {
     name: "CSharp",
     description:
       "C# é uma linguagem de programação desenvolvida pela Microsoft, usada principalmente para desenvolvimento de software na plataforma .NET.",
     helloWorld: "Console.WriteLine('Olá, Mundo!');",
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/csharp",
-    linkDocumentacao: "https://docs.microsoft.com/pt-br/dotnet/csharp/",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/csharp",
+        docs: "https://docs.microsoft.com/pt-br/dotnet/csharp/",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=tJVFOR08QJg&list=PLPc-V1ujthioJ8Cq_yMzYAbeSvaPye-aa",
+      ],
+    },
   },
   {
     name: "Ruby",
     description:
       "Ruby é uma linguagem de programação dinâmica e de alto nível, conhecida por sua simplicidade e produtividade.",
     helloWorld: "puts 'Olá, Mundo!'",
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/ruby",
-    linkDocumentacao: "https://www.ruby-lang.org/pt/documentation/",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/ruby",
+        docs: "https://www.ruby-lang.org/pt/documentation/",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=2js9Q_BMD-8&list=PLdDT8if5attEOcQGPHLNIfnSFiJHhGDOZ",
+      ],
+    },
   },
   {
     name: "PHP",
     description:
       "PHP é uma linguagem de script amplamente utilizada para desenvolvimento web, especialmente em servidores.",
     helloWorld: "echo 'Olá, Mundo!';",
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/php",
-    linkDocumentacao: "https://www.php.net/docs.php",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/php",
+        docs: "https://www.php.net/docs.php",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=TfsO0BGvGn0&list=PLHz_AreHm4dlFPrCXCmd5g92860x_Pbr_",
+      ],
+    },
   },
   {
     name: "Swift",
     description:
       "Swift é uma linguagem de programação desenvolvida pela Apple para desenvolvimento de aplicativos iOS e macOS.",
     helloWorld: "print('Olá, Mundo!')",
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/swift",
-    linkDocumentacao: "https://developer.apple.com/documentation/swift",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/swift",
+        docs: "https://developer.apple.com/documentation/swift",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=MnL3-a3eKNQ&list=PLJ0AcghBBWShgIH122uw7H9T9-NIaFpP-",
+      ],
+    },
   },
   {
     name: "Go",
     description:
       "Go, também conhecida como Golang, é uma linguagem de programação desenvolvida pelo Google, focada em simplicidade e eficiência.",
     helloWorld: "fmt.Println('Olá, Mundo!')",
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/go",
-    linkDocumentacao: "https://golang.org/doc/",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/golang",
+        docs: "https://golang.org/doc/",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=WiGU_ZB-u0w&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg",
+      ],
+    },
   },
   {
     name: "Kotlin",
     description:
       "Kotlin é uma linguagem de programação moderna que é totalmente interoperável com Java e é usada principalmente para desenvolvimento Android.",
     helloWorld: "fun main() { println('Olá, Mundo!') }",
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/kotlin",
-    linkDocumentacao: "https://kotlinlang.org/docs/home.html",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/kotlin",
+        docs: "https://kotlinlang.org/docs/home.html",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=4lOjDziyce8&list=PLJ0AcghBBWSj0i1SQfr3GdwUBnUaIf5xw",
+      ],
+    },
   },
   {
     name: "Rust",
     description:
       "Rust é uma linguagem de programação de sistemas que enfatiza segurança e desempenho.",
     helloWorld: 'fn main() { println!("Olá, Mundo!"); }',
-    linkEstudo: "https://www.alura.com.br/cursos-online-programacao/rust",
-    linkDocumentacao: "https://doc.rust-lang.org/book/",
+    links: {
+      schools: {
+        alura: "https://www.alura.com.br/cursos-online-programacao/rust",
+        docs: "https://doc.rust-lang.org/book/",
+      },
+      playlists: [
+        "https://www.youtube.com/watch?v=Hizsjpi66D8&list=PLWmXJQDlXOHX6UdAmXv6euoqDPUtMLpJf",
+      ],
+    },
   },
 ];
